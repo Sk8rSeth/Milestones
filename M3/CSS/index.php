aad2fe636e3583a220735969876ff43bcc1e2e8f
Sk8rSeth <sethhowell2197@gmail.com>
2015-01-28 19:09:05 -0700

    eod m3 css start

---
+++ M3/CSS/index.php
@@ -0,0 +1,32 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>CSS Site</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="hero">
+		<div class="header-container">
+			<div class="logo">
+				<a href="/CSS/">puppy love</a>
+			</div>
+			<div class="searchbar">
+				search
+			</div>
+			<nav>
+				<span>menu</span>
+				<span>menu</span>
+				<span>menu</span>
+				<span>menu</span>
+				<span>menu</span>
+			</nav>
+		</div>
+		<div class="blankspace">
+			lower
+		</div>
+	</div>
+</body>
+</html>
