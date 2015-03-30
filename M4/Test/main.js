42e00cd12e638820e6dbff885e88eae7cdcd4503
Sk8rSeth <sethhowell2197@gmail.com>
2015-03-30 13:29:45 -0700

    updated push for resumes

--- M4/Test/main.js
+++ M4/Test/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	var feed = new Instafeed({
 			get: 'tagged',
-			tagName: 'pitbulllove',
+			tagName: 'echocanyonHTZ',
 			clientId: 'ad19ab7ad8e940a5a8ee3775ac7553ff',
 			template: '<img src={{image}}>'
 		});
